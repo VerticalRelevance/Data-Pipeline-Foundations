ef289ee4723d30108625ee8c9491b34d58b4f1d7
smurthy4 <89792352+smurthy4@users.noreply.github.com>
2022-07-21 11:50:55 -0500

    Update run_dms_task.py

--- Data Pipeline Foundations/supplementary_scripts/run_dms_task.py
+++ Data Pipeline Foundations/supplementary_scripts/run_dms_task.py
@@ -1,7 +1,7 @@
 import boto3
 
 #Enter replication task ARN
-replication_task_arn = "arn:aws:dms:us-west-1:899456967600:task:3PI6UXFCV5C62LJNCS33PKJTIEMHBGAM4PPLWCI"
+replication_task_arn = ""
 
 dms = boto3.client('dms')
 
